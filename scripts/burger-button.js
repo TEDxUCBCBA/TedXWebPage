ab728080a1c280caf7af7ff3b2dd3c7db445e08c
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-05-08 11:00:23 -0400

    #68 | implemented burger menu

---
+++ scripts/burger-button.js
@@ -0,0 +1,39 @@
+
+let showed = false;
+const burgerButton = document.querySelector('.burger-button ');
+const menu = document.querySelector('.menu');
+
+
+function controlVerticalMenu(){
+    if (showed === false) {
+        menu.classList.remove('none');
+        showed = true;
+    }else {
+        menu.classList.add('none');
+        showed = false;
+    }
+}
+
+
+function addVerticalMenuHiden() {
+    menu.classList.add('vertical-menu');
+    menu.classList.add('none');
+}
+
+function removeVerticalMenuHiden() {
+    menu.classList.remove('vertical-menu');
+    menu.classList.remove('none');
+}
+
+function query(x) {
+    if (x.matches) {
+        addVerticalMenuHiden();
+    }else {
+        removeVerticalMenuHiden();
+    }
+}
+
+let x = window.matchMedia("(max-width: 1200px)");
+query(x);
+x.addListener(query);
+burgerButton.addEventListener('click', controlVerticalMenu)
