3d375cdc96375b8a8a856ca584132054de07234e
Rodrigo Torrico Del Castillo <rodrigo.torricodelcastillo@gmail.com>
2018-07-02 14:57:03 -0400

    images optimized

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,14 @@
 
 
-$( document ).ready(function() {
+
+$(window).on("load", function() {
     const loader = document.querySelector('.is-page-loader');
     const nav = document.querySelector('nav');
     const content = document.querySelector('#container');
-
+    
     loader.classList.add('hide');
     nav.classList.remove('hide');
     content.classList.remove('hide');
-
-    particlesJS.load('particles-js', './scripts/particles.json', function() {});
     
+    particlesJS.load('particles-js', './scripts/particles.json', function() {});
 });
