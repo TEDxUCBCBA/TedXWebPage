839b5f6d4ad1cfa38b69e7ff40a278dd30175522
D. Leandro Guardia V <lean.guardia93@gmail.com>
2018-07-03 01:56:39 -0400

    115 - Save the date and Remove some map controls

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,18 +1,19 @@
 
-
 function myMap() {
     
     var ucb = new google.maps.LatLng(-17.371794, -66.143639);
 
     const map = document.getElementById("googleMap");
 
-    let mapProp= {
+    let mapProp = {
         center: ucb,
         zoom:17,
+        mapTypeControl: false,
+        scaleControl: false
     };
 
     let myMap = new google.maps.Map(map, mapProp);
 
     let marker = new google.maps.Marker({position:ucb});
     marker.setMap(myMap);
-}
+};
